deba21c27027c099511d81b20dd5ab601369a562
ssaavedraa <=>
2023-07-12 07:46:51 -0500

    fix card icon styling

--- src/app/compontents/Card/styles.ts
+++ src/app/compontents/Card/styles.ts
@@ -17,7 +17,7 @@ export interface CardStyles {
 export const cardStyles: CardStyles = {
   grid: {
     card: 'min-w-[300px] sm:min-w-[351px] h-[300px] sm:h-auto relative mr-4t bg-gradient-to-t from-[#0000009d] to-transparent mr-5 sm:m-0 aspect-square',
-    cardIcon: 'bg-yellow-500 w-8 h-8 absolute top-[90px] flex items-center',
+    cardIcon: 'w-8 h-8 absolute top-[90px] flex items-center',
     image: 'absolute w-full h-full -z-10',
     dataContainer: 'h-full flex flex-col justify-end pb-9 m-auto px-8',
     name: 'font-regular text-3xl line-clamp-2',
@@ -27,7 +27,7 @@ export const cardStyles: CardStyles = {
   },
   list: {
     card: 'min-w-full card-gradient flex flex-row justify-end relative h-[138px] overflow-y-clip my-2',
-    cardIcon: 'bg-yellow-500 w-8 h-8 absolute top-0 left-0 flex items-center',
+    cardIcon: 'w-8 h-8 absolute top-0 left-0 flex items-center',
     image: 'absolute w-[180px] -left-[15px] top-[calc(50%-95px)] -z-10',
     dataContainer: 'w-[370px] mr-[232px]',
     name: 'text-[28px] font-regular line-clamp-1',
