dc0bc473987d6d31f7c4108bbfdd41b7b34f0e85
ssaavedraa <=>
2023-07-12 08:12:16 -0500

    increase accesibility

--- src/app/compontents/VotingBox/VotingBox.tsx
+++ src/app/compontents/VotingBox/VotingBox.tsx
@@ -14,13 +14,13 @@ interface VotingBoxProps {
 export default function VotingBox ({ styles, isButtonSelected, handleVoteSelection, sendButtonMessage, sendVote, vote }: VotingBoxProps) {
   return (
     <div className={styles}>
-      <button className={`bg-teal-500 w-8 h-8 mx-[6px] disabled:opacity-60 ${isButtonSelected('positive') ? 'border-2 border-white' : ''}`} value='positive' disabled={sendButtonMessage === 'Vote Again'} onClick={() => handleVoteSelection('positive')}>
+      <button className={`bg-teal-500 w-8 h-8 mx-[6px] disabled:opacity-60 ${isButtonSelected('positive') ? 'border-2 border-white' : ''}`} value='positive' disabled={sendButtonMessage === 'Vote Again'} onClick={() => handleVoteSelection('positive')} title='vote positive'>
         <FaThumbsUp className='m-auto'/>
       </button>
-      <button className={`bg-yellow-500 w-8 h-8 mx-[6px] disabled:opacity-60 ${isButtonSelected('negative') ? 'border-2 border-white' : ''}`} value='negative' disabled={sendButtonMessage === 'Vote Again'} onClick={() => handleVoteSelection('negative')}>
+      <button className={`bg-yellow-500 w-8 h-8 mx-[6px] disabled:opacity-60 ${isButtonSelected('negative') ? 'border-2 border-white' : ''}`} value='negative' disabled={sendButtonMessage === 'Vote Again'} onClick={() => handleVoteSelection('negative')} title='vote negative'>
         <FaThumbsDown className='m-auto'/>
       </button>
-      <button className='text-center px-3 py-1 text-lg bg-black bg-opacity-60 border-white border-[1px] min-w-[107px] h-[38px] ml-[6px] from-[#000000ab] disabled:opacity-80' disabled={!vote && sendButtonMessage === 'Vote Now'} onClick={sendVote}>
+      <button className='text-center px-3 py-1 text-lg bg-black bg-opacity-60 border-white border-[1px] min-w-[107px] h-[38px] ml-[6px] from-[#000000ab] disabled:opacity-80' disabled={!vote && sendButtonMessage === 'Vote Now'} onClick={sendVote} title={sendButtonMessage}>
         {sendButtonMessage}
       </button>
     </div>
